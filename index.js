b12bb26e5669c2c735ff21e80f84c4f619e45c10
Corey Butler <corey@coreybutler.com>
2016-06-28 14:53:35 -0500

    Updated to pure ES2015 syntax. Requires Node 6+.

--- index.js
+++ index.js
@@ -1,260 +1,338 @@
+'use strict'
+
 /**
+ * @class shortbus
  * Shortbus accepts an optional parameter to determine the level of logging to use.
  *
  * ex: var tasks = new ShortBus('development');
  * ex: var tasks = new ShortBus('dev');
  *
  * The example above will generate more verbose output.
+ * @extend events.EventEmitter
  */
-var ShortBus = function(){
-  var me = this; // Handle that pesky scope
+const EventEmitter = require('events').EventEmitter
+const QueueItem = require('./lib/queueitem')
 
-  this.steps = []; // Holds the functions
-  this.completed = 0; // The number of functions which have completed processing.
-  this.timeout = null;
+class ShortBus extends EventEmitter {
+  constructor () {
+    super()
 
-  var mode = arguments[0] || process.env.NODE_ENV || 'production';
-
-  Object.defineProperties(this,{
-    mode: {
-      enumerable: true,
-      get: function(){ return mode; },
-      set: function(v){
-        if (mode.toLowerCase().substr(0,3) === 'dev') {
-          mode = 'dev';
-        } else {
-          mode = 'prod';
-        }
-      }
-    },
-    processing: {
-      enumerable: false,
-      writable: true,
-      configurable: false,
-      value: false
-    },
-    list: {
-      enumerable: true,
-      get: function(){
-        return this.steps.map(function(s){
-          return {
-            id: s.id,
-            name: s.name,
-            status: s.status
-          };
-        });
-      }
-    },
-    timer: {
-      enumerable: false,
-      writable: true,
-      configurable: false,
-      value: null
-    },
-    onTimeout: {
-      enumerable: false,
-      writable: false,
-      configurable: false,
-      value: function(){
-        var log = [];
-        if (me.steps.length>0){
-          me.steps.forEach(function(s){
-            log.push(s.name,s.status===null?'NOT STARTED':s.status);
-          });
-        }
-        me.emit('timeout',{process:log});
-//        throw new Error('Timed out.');
-//        process.exit(1);
-      }
-    }
-  });
-
-  this.on('stepcomplete',function(step){
-
-    // Disallow duplicates
-    if (step.status === 'completed'){
-      return;
-    }
-
-    step._status = 'complete';
-
-    // When the step is done, tally it
-    me.completed++;
-    me.mode === 'dev' && console.log(step.name+' completed'+'.\n---------------------------------');
-
-    // If all of the queries have been tallied, we're done.
-    if (me.completed === me.steps.length) {
-      me.processing = false;
-      Object.keys(me.steps).forEach(function(step){
-        clearTimeout(me.steps[step].timer);
-      });
-      me.emit('complete');
-    }
-  });
-
-  this.mode = mode;
-
-  // Add a step to the queue.
-  this.add = function(name,fn) {
-    if (this.processing){
-      return console.warn('Cannot add a step while processing.');
-    }
-    if (typeof name === 'function'){
-      fn = name;
-      name = 'Step '+(parseInt(me.steps.length)+1);
-    }
-    if (fn === undefined || fn === null){
-      console.error('No processing method defined for step '+(parseInt(this.steps.length)+1)+'.');
-      process.exit(1);
-    }
-
-    var queue = {
-      name: name,
-      method: function(){
-        var self = this,
-            x = {
-              timeout: function(milliseconds){
-                queue.timer = setTimeout(function(){
-                  if (me.processing) {
-                    me.emit('steptimeout',queue);
-                  }
-                }, milliseconds);
-              }
-            };
-        fn.apply(x,arguments);
-      },
-      _status: null
-    };
-
-    Object.defineProperties(queue,{
-      id: {
-        enumerable: true,
-        writable: false,
-        configurable: false,
-        value:  (this.steps.length > 0 ? this.steps[this.steps.length-1].id : 0)+1
-      },
-      async: {
+    Object.defineProperties(this, {
+      steps: {
         enumerable: false,
-        writable: false,
+        writable: true,
         configurable: false,
-        value: (fn && fn.length > 0)
+        value: []
+      },
+      completed: {
+        enumerable: false,
+        writable: true,
+        configurable: false,
+        value: 0
+      },
+      timeout: {
+        enumerable: false,
+        writable: true,
+        configurable: false,
+        value: null
+      },
+      _mode: {
+        enumerable: false,
+        writable: true,
+        configurable: false,
+        value: arguments[0] || process.env.NODE_ENV || 'production'
+      },
+      processing: {
+        enumerable: false,
+        writable: true,
+        configurable: false,
+        value: false
       },
       timer: {
         enumerable: false,
         writable: true,
         configurable: false,
         value: null
-      },
-      status: {
-        enumerable: false,
-        get: function(){
-          return this._status;
-        }
       }
-    });
+    })
 
-    this.steps.push(queue);
-    this.emit('addstep',queue);
-  };
+    const me = this
+    this.on('stepcomplete', function (step) {
+      // Disallow duplicates
+      if (step.status === 'completed') {
+        return
+      }
 
-  // Get a specific step by index
-  this.getAt = function(i){
-    return this.steps[i];
-  };
+      step._status = 'complete'
 
-  // Get a specific step by name or ID
-  this.get = function(step){
+      // When the step is done, tally it
+      me.completed++
+      if (me.mode === 'dev') {
+        console.log(step.name + ' completed' + '.\n---------------------------------')
+      }
+
+      // If all of the queries have been tallied, we're done.
+      if (me.completed === me.steps.length) {
+        me.processing = false
+
+        Object.keys(me.steps).forEach(function (step) {
+          clearTimeout(me.steps[step].timer)
+        })
+
+        me.emit('complete')
+      }
+    })
+  }
+
+  get list () {
+    return this.steps.map(function (s) {
+      return {
+        id: s.number,
+        name: s.name,
+        status: s.status
+      }
+    })
+  }
+
+  /**
+   * @property {string} mode
+   * The type of processing (dev, prod, etc). Setting this to
+   * `dev` enables verbose logging.
+   */
+  get mode () {
+    return this._mode
+  }
+
+  set mode (value) {
+    if (value.toLowerCase().substr(0, 3) === 'dev') {
+      this._mode = 'dev'
+    } else {
+      this._mode = 'prod'
+    }
+  }
+
+  onTimeout () {
+    let log = []
+    if (this.steps.length > 0){
+      this.steps.forEach(function (s) {
+        log.push(s.name, s.status === null ? 'NOT STARTED' : s.status)
+      })
+    }
+    this.emit('timeout',{
+      process: log
+    })
+  }
+
+  /**
+   * @method add
+   * @param {string} [name]
+   * A descriptive name for the queued process/task.
+   * @param {function} callback
+   * The function to queue.
+   * @param {function} callback.next
+   * This argument allows users to explicitly use asynchronous
+   * methods. Example:
+   *
+   * ```
+   * let ShortBus = require('shortbus')
+   * let tasks = new ShortBus()
+   *
+   * tasks.add('Descriptive Title', function (next) {
+   *   myAsyncMethod(function () {
+   *     console.log('Ran something async.')
+   *     next()
+   *   })
+   * })
+   */
+  add (name, fn) {
+    if (this.processing) {
+      return console.warn('Cannot add a step while processing.')
+    }
+
+    if (typeof name === 'function') {
+      fn = name
+      name = 'Step ' + (parseInt(this.steps.length) + 1)
+    }
+
+    if (typeof fn !== 'function') {
+      throw new Error('No processing method defined for step ' + (parseInt(this.steps.length) + 1) + '.')
+    }
+
+    const queue = new QueueItem({
+      name: name,
+      callback: fn,
+      number: (this.steps.length > 0 ? this.steps[this.steps.length-1].number : 0) + 1
+    })
+
+    const me = this
+    queue.on('stepcomplete', function (step) {
+      me.emit('stepcomplete', step)
+    })
+    queue.on('steptimeout', function (step) {
+      if (step.status === 'running') {
+        me.emit('steptimeout', step)
+      }
+    })
+
+    this.steps.push(queue)
+    this.emit('addstep', queue)
+  }
+
+  /**
+   * @method getAt
+   * @param  {number} index
+   * Retrieve a queue item by it's index/queue number.
+   * @return {Queue}
+   */
+  getAt (index) {
+    return this.steps[index]
+  }
+
+  /**
+   * @method get
+   * Retrieve a specific queue item.
+   * @param  {string} requestedStepTitle
+   * The descriptie name of the queue item to retrieve.
+   * @return {Queue}
+   */
+  get (requestedStep) {
     // Get by Name
-    var el = me.steps.filter(function(s){
-      return s.name === step;
-    });
-    if (el.length === 1){
-      return el[0];
-    }
-    // Get by ID
-    var el = me.steps.filter(function(s){
-      return s.id === step;
-    });
-    if (el.length === 1){
-      return el[0];
-    }
-  };
+    let element = this.steps.filter(function (step) {
+      return step.name === requestedStep
+    })
 
-  // Remove a step by name or ID. Seeks in that order
-  this.remove = function(step){
-    if (this.processing){
-      return console.warn('Cannot add a step while processing.');
+    if (element.length === 1) {
+      return element[0]
     }
+
+    // Get by index
+    element = this.steps.filter(function (step) {
+      return step.number === requestedStep
+    })
+
+    if (element.length === 1) {
+      return element[0]
+    }
+  }
+
+  /**
+   * @method remove
+   * Remove a queue item by name or number.
+   * @param  {string} requestedStepTitle
+   * The descriptie name of the queue item to retrieve.
+   * @return {Queue}
+   * Returns the item that was removed.
+   */
+  remove (requestedStep) {
+    if (this.processing) {
+      return console.warn('Cannot add a step while processing.')
+    }
+
     // Remove by name
-    var el = me.steps.filter(function(s){
-      return s.name === step;
-    });
-    if (el.length === 1){
-      me.steps = me.steps.filter(function(s){
-        return s.name !== step;
-      });
-      return el[0];
+    let element = this.steps.filter(function (step) {
+      return step.name === requestedStep
+    })
+
+    if (element.length === 1) {
+      this.steps = this.steps.filter(function (step) {
+        return step.name !== requestedStep
+      })
+
+      return element[0]
     }
+
     // Remove by ID
-    var el = me.steps.filter(function(s){
-      return s.id === step;
-    });
-    if (el.length === 1){
-      me.steps = me.steps.filter(function(s){
-        return s.id !== step;
-      });
-      return el[0];
-    }
-  };
+    element = this.steps.filter(function (step) {
+      return step.number === requestedStep
+    })
 
-  // Remove at a specific array index
-  this.removeAt = function(step){
-    if (this.processing){
-      return console.warn('Cannot add a step while processing.');
+    if (element.length === 1) {
+      this.steps = this.steps.filter(function (step) {
+        return step.number !== requestedStep
+      })
+      return element[0]
     }
+  }
+
+  /**
+   * @method removeAt
+   * Removes a queue item from the specific index.
+   * @param  {number} requestedStepIndex
+   * The queue index/number.
+   * @return {Queue}
+   * Returns the item that was removed.
+   */
+  removeAt (requestedStep) {
+    if (this.processing) {
+      return console.warn('Cannot add a step while processing.')
+    }
+
     // Remove by index
-    if (typeof step !== 'number'){
-      return console.error('Failed to remove step: '+step);
+    if (typeof requestedStep !== 'number') {
+      return console.error('Failed to remove step: ' + requestedStep)
     }
-    if (step < 0 || step >= me.steps.length){
-      return console.error('Step index '+step+' could not be found or does not exist.');
-    }
-    return me.steps.splice(step,1)[0];
-  };
 
-  // Run the queued processes in order
-  this.process = function(){
-    if (this.processing){
-      return console.warn('Cannot start processing (already running). Please wait for this process to complete before calling process() again.');
+    if (requestedStep < 0 || requestedStep >= this.steps.length) {
+      return console.error('Step index ' + requestedStep + ' could not be found or does not exist.')
     }
-    me.processing = true;
-    if (me.timeout !== null){
-      me.timer = setTimeout(me.onTimeout,me.timeout);
+
+    return this.steps.splice(requestedStep, 1)[0]
+  }
+
+  /**
+   * @method process
+   * Run the queued process in order
+   * @param {boolean} [sequential=false]
+   * Set to `true` to run the queue items in a synchronous-like manner.
+   * This will execute each method one after the other. Each method must
+   * complete before the next is started.
+   */
+  process (sequential) {
+    if (this.processing) {
+      return console.warn('Cannot start processing (already running). Please wait for this process to complete before calling process() again.')
     }
-    for (var i=0; i< this.steps.length; i++){
-      me.mode === 'dev' && console.log('\n---------------------------------\nExecuting '+me.steps[i].name+':');
-      me.steps[i]._status = 'running';
 
-      var step = me.steps[i];
+    this.processing = true
 
-      if (step.async){
-        var cb = function(){
-          me.emit('stepcomplete',step);
-        };
-      } else {
-        var cb = setTimeout(function(){
-          me.emit('stepcomplete',step);
-        },1);
+    if (this.timeout !== null) {
+      const me = this
+      this.timer = setTimeout(function () {
+        me.onTimeout()
+      }, this.timeout)
+    }
+
+    sequential = typeof sequential === 'boolean' ? sequential : false
+    if (!sequential) {
+      for (var i = 0; i < this.steps.length; i++) {
+        if (this.mode === 'dev') {
+          console.log('\n---------------------------------\nExecuting ' + this.steps[i].name + ':')
+        }
+        this.steps[i].run()
       }
+    } else {
+      let queue = this.steps
+      let listener = new EventEmitter()
+      const me = this
 
-      step.method.call(step,cb);
+      listener.on('stepcomplete', function () {
+        if (queue.length > 0) {
+          const currentStep = queue.shift()
+          currentStep.on('stepcomplete', function () {
+            listener.emit('stepcomplete')
+          })
+          currentStep.run()
+        } else {
+          me.emit('complete')
+        }
+      })
+
+      let currentStep = queue.shift()
+      currentStep.on('stepcomplete', function () {
+        listener.emit('stepcomplete')
+      })
+      currentStep.run()
     }
-  };
+  }
+}
 
-};
-
-// Inherit the even emitter awesome sauce from the native node modules
-var EventEmitter = require('events').EventEmitter;
-require('util').inherits(ShortBus,EventEmitter);
-
-module.exports = ShortBus;
+module.exports = ShortBus
--- test/index.js
+++ test/index.js
@@ -1,169 +1,220 @@
-"use strict";
+"use strict"
 
 var ShortBus = require('../index'),
-    assert = require('assert');
+    assert = require('assert')
 
-suite('Queue',function(){
+suite('Queue',function () {
 
-  var tasks;
+  var tasks
 
-  setup(function(){
-    tasks = new ShortBus('dev');
-  });
+  setup(function () {
+    tasks = new ShortBus('dev')
+  })
 
-  test('Add Task',function(){
-    tasks.add(function(){
-      console.log('Task 1');
-    });
-    tasks.add('t2',function(){
-      console.log('Task 2');
-    });
-    tasks.add(function(){
-      console.log('Task 3');
-    });
-    assert.ok(tasks.list.length === 3, 'Invalid number of steps queued. Expected: 3, Actual: '+tasks.list.length);
-    assert.ok(tasks.list[0].name === 'Step 1', 'Autoname failed. Expected: Step 1, Actual: '+tasks.list[1].name);
-    assert.ok(tasks.list[1].name === 't2', 'Invalid step name. Expected: t2, Actual: '+tasks.list[1].name);
-  });
+  test('Add Task',function () {
+    tasks.add(function () {
+      console.log('Task 1')
+    })
+    tasks.add('t2',function () {
+      console.log('Task 2')
+    })
+    tasks.add(function () {
+      console.log('Task 3')
+    })
+    assert.ok(tasks.list.length === 3, 'Invalid number of steps queued. Expected: 3, Actual: '+tasks.list.length)
+    assert.ok(tasks.list[0].name === 'Step 1', 'Autoname failed. Expected: Step 1, Actual: '+tasks.list[1].name)
+    assert.ok(tasks.list[1].name === 't2', 'Invalid step name. Expected: t2, Actual: '+tasks.list[1].name)
+  })
 
-  test('Remove Task', function(){
-    tasks.add(function(){
-      console.log('Task 1');
-    });
-    tasks.add('t2',function(){
-      console.log('Task 2');
-    });
-    tasks.add('t3',function(){
-      console.log('Task 3');
-    });
-    var x = tasks.removeAt(1);
-    assert.ok(x.id === 2, 'Unrecognized task removed. Expected ID #2, Actual ID: '+x.id);
-    assert.ok(tasks.list.length === 2, 'Invalid number of steps queued. Expected: 2, Actual: '+tasks.list.length);
-    assert.ok(tasks.list[1].name === 't3', 'Invalid step name. Expected: t3, Actual: '+tasks.list[1].name);
-    x = tasks.remove('t3');
-    assert.ok(x.id === 3, 'Unrecognized task removed. Expected ID #3, Actual ID: '+x.id);
-    assert.ok(tasks.list.length === 1, 'Invalid number of steps queued. Expected: 1, Actual: '+tasks.list.length);
-    assert.ok(tasks.list[0].name === 'Step 1', 'Invalid step name. Expected: Step 1, Actual: '+tasks.list[0].name);
-    x = tasks.remove(1);
-    assert.ok(x.id === 1, 'Unrecognized task removed. Expected ID #1, Actual ID: '+x.id);
-    assert.ok(tasks.list.length === 0, 'Invalid number of steps queued. Expected: None, Actual: '+tasks.list.length);
-  });
+  test('Remove Task', function () {
+    tasks.add(function () {
+      console.log('Task 1')
+    })
+    tasks.add('t2',function () {
+      console.log('Task 2')
+    })
+    tasks.add('t3',function () {
+      console.log('Task 3')
+    })
+    var x = tasks.removeAt(1)
+    assert.ok(x.number === 2, 'Unrecognized task removed. Expected ID #2, Actual ID: '+x.number)
+    assert.ok(tasks.list.length === 2, 'Invalid number of steps queued. Expected: 2, Actual: '+tasks.list.length)
+    assert.ok(tasks.list[1].name === 't3', 'Invalid step name. Expected: t3, Actual: '+tasks.list[1].name)
+    x = tasks.remove('t3')
+    assert.ok(x.number === 3, 'Unrecognized task removed. Expected ID #3, Actual ID: '+x.number)
+    assert.ok(tasks.list.length === 1, 'Invalid number of steps queued. Expected: 1, Actual: '+tasks.list.length)
+    assert.ok(tasks.list[0].name === 'Step 1', 'Invalid step name. Expected: Step 1, Actual: '+tasks.list[0].name)
+    x = tasks.remove(1)
+    assert.ok(x.number === 1, 'Unrecognized task removed. Expected ID #1, Actual ID: '+x.number)
+    assert.ok(tasks.list.length === 0, 'Invalid number of steps queued. Expected: None, Actual: '+tasks.list.length)
+  })
 
-  test('Retrieve a task.', function(){
-    tasks.add(function(){
-      console.log('Task 1');
-    });
-    tasks.add('t2',function(){
-      console.log('Task 2');
-    });
-    tasks.add('t3',function(){
-      console.log('Task 3');
-    });
-    var t = tasks.getAt(1);
-    assert.ok(t.id === 2,'tasks.getAt method returned unexpected value. Expected ID: 2, Received: '+t.id);
-    t = tasks.get('t3');
-    assert.ok(t.id === 3,'tasks.get (by name) method returned unexpected value. Expected ID: 3, Received: '+t.id);
-    t = tasks.get(2);
-    assert.ok(t.id === 2,'tasks.get (by ID) method returned unexpected value. Expected ID: 2, Received: '+t.id);
-  });
+  test('Retrieve a task.', function () {
+    tasks.add(function () {
+      console.log('Task 1')
+    })
+    tasks.add('t2',function () {
+      console.log('Task 2')
+    })
+    tasks.add('t3',function () {
+      console.log('Task 3')
+    })
+    var t = tasks.getAt(1)
+    assert.ok(t.number === 2,'tasks.getAt method returned unexpected value. Expected ID: 2, Received: '+t.number)
+    t = tasks.get('t3')
+    assert.ok(t.number === 3,'tasks.get (by name) method returned unexpected value. Expected ID: 3, Received: '+t.number)
+    t = tasks.get(2)
+    assert.ok(t.number === 2,'tasks.get (by ID) method returned unexpected value. Expected ID: 2, Received: '+t.number)
+  })
 
-});
+})
 
-suite('Processing', function(){
-  var tasks;
+suite('Processing', function () {
+  var tasks
 
-  setup(function(){
-    tasks = new ShortBus();
-  });
+  setup(function () {
+    tasks = new ShortBus()
+  })
 
   test('Simple linear execution.',function(done){
-    var x = [];
+    var x = []
 
-    tasks.add(function(){
-      x.push('Task 1');
-    });
-    tasks.add('t2',function(){
-      x.push('Task 2');
-    });
-    tasks.add('t3',function(){
-      x.push('Task 3');
-    });
+    tasks.add(function () {
+      x.push('Task 1')
+    })
+    tasks.add('t2',function () {
+      x.push('Task 2')
+    })
+    tasks.add('t3',function () {
+      x.push('Task 3')
+    })
 
-    tasks.on('complete',function(){
-      assert.ok(x.length === 3, 'Invalid result.'+x.toString());
-      done();
-    });
-    tasks.process();
-  });
+    tasks.on('complete',function () {
+      assert.ok(x.length === 3, 'Invalid result.'+x.toString())
+      done()
+    })
+    tasks.process()
+  })
 
   test('Async execution.', function(done){
-    this.timeout(4000);
-    var x = [];
-    tasks.add(function(){
-      x.push('Task 1');
-    });
-    tasks.add('t2',function(next){
-      setTimeout(function(){
-        x.push('Task 2');
-        next();
-      },700);
-    });
-    tasks.add('t3',function(){
-      x.push('Task 3');
-    });
+    this.timeout(4000)
+    var x = []
+    tasks.add(function () {
+      x.push('Task 1')
+    })
+    tasks.add('t2a',function(next){
+      setTimeout(function () {
+        x.push('Task 2')
+        next()
+      },700)
+    })
+    tasks.add('t3',function () {
+      x.push('Task 3')
+    })
 
-    tasks.on('complete',function(){
-      assert.ok(x.length === 3, 'Invalid result.'+x.toString());
-      done();
-    });
-    tasks.process();
-  });
+    tasks.on('complete',function () {
+      assert.ok(x.length === 3, 'Invalid result.'+x.toString())
+      done()
+    })
+    tasks.process()
+  })
 
   test('Process timeout.',function(done){
-    this.timeout(500);
+    this.timeout(500)
 
-    tasks.timeout = .00000000001;
+    tasks.timeout = .00000000001
 
-    var x = [];
-    tasks.add(function(){
-      x.push('Task 1');
-    });
+    var x = []
+    tasks.add(function () {
+      x.push('Task 1')
+    })
     tasks.add('t2',function(next){
-      setTimeout(function(){
-        x.push('Task 2');
-        next();
-      },700);
-    });
+      setTimeout(function () {
+        x.push('Task 2')
+        next()
+      },700)
+    })
 
-    tasks.on('timeout', function(){
-      done();
-    });
+    tasks.on('timeout', function () {
+      done()
+    })
 
-    tasks.process();
+    tasks.process()
 
-  });
+  })
 
   test('Task timeout.',function(done){
 
-    this.timeout(3000);;
+    this.timeout(3000)
 
-    var x = [];
-    tasks.add(function(){
-      x.push('Task 1');
-    });
+    var x = []
+    tasks.add(function () {
+      x.push('Task 1')
+    })
     tasks.add('t2',function(next){
-      this.timeout(500);
-      setTimeout(function(){
-        x.push('Task 2');
-        next();
-      },2000);
-    });
+      this.timeout(500)
+      setTimeout(function () {
+        x.push('Task 2')
+        next()
+      },2000)
+    })
 
-    tasks.on('steptimeout', function(){
-      done();
-    });
+    tasks.on('steptimeout', function () {
+      done()
+    })
 
-    tasks.process();
+    tasks.process()
 
-  });
-});
+  })
+})
+
+suite('Sequential Processing', function () {
+  var tasks
+
+  setup(function () {
+    tasks = new ShortBus()
+  })
+
+  test('Simple linear execution.',function(done){
+    var x = []
+
+    tasks.add(function () {
+      x.push(1)
+    })
+    tasks.add('t2',function () {
+      x.push(2)
+    })
+    tasks.add('t3',function () {
+      x.push(3)
+    })
+
+    tasks.on('complete',function () {
+      assert.ok(x[0] === 1 && x[1] === 2 && x[2] === 3, 'Invalid result.'+x.toString())
+      done()
+    })
+    tasks.process(true)
+  })
+
+  test('Asynchronous sequential execution.', function (done) {
+    var x = []
+
+    tasks.add(function () {
+      x.push(1)
+    })
+    tasks.add('t2',function (next) {
+      setTimeout(function () {
+        x.push(2)
+        next()
+      }, 600)
+    })
+    tasks.add('t3',function () {
+      x.push(3)
+    })
+
+    tasks.on('complete',function () {
+      assert.ok(x[0] === 1 && x[1] === 2 && x[2] === 3, 'Invalid result.'+x.toString())
+      done()
+    })
+    tasks.process(true)
+  })
+})
